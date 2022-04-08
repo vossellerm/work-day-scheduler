8565fdf31655ca949b62280d04637b40f07000d3
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-07 21:09:04 -0700

    add current date

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 // current day: Day, Month Day(th)
+// Thu Apr 07 2022 20:55:35 GMT-0700 (Pacific Daylight Time)
+// Moment Format: ddd MMM DD YYYY kk:mm:ss zz
+var currentDay = document.getElementById("currentDay");
+var date = new Date();
+var reformDate = moment(date, "ddd MMM DD YYYY kk:mm:ss zz").format(
+  "dddd, MMMM Do"
+);
+currentDay.textContent = reformDate;
 
 // time blocks for standard business hours (9am-pm)
 // IF past, grey
