a729b32345682dd5f68db68609419b4250cdfd73
Mikayla Vosseller <vossellerm@gmail.com>
2022-04-07 20:42:06 -0700

    pseudocode

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+// current day: Day, Month Day(th)
+
+// time blocks for standard business hours (9am-pm)
+// IF past, grey
+// IF present, red
+// IF future, green
+// Hour on left
+// text box in middle
+// save button on right, saves text in local storage
+// saved events persist when page is refreshed
